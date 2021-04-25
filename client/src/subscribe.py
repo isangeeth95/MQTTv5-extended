89c1789583b7158320f97073d7e1cc73b8830009
isangeeth95 <isangeeth95@gmail.com>
2021-04-25 18:40:45 +0000

    subscribe test script

--- client/src/subscribe.py
+++ client/src/subscribe.py
@@ -1,5 +1,5 @@
-import paho.mqtt.subscribe as subscribe
-#from paho.mqtt import subscribe as subscribe
+#import paho.mqtt.subscribe as subscribe
+from paho.mqtt import subscribe as subscribe
 
 msg = subscribe.simple("house/main-light", hostname="10.0.2.4")
 print("%s %s" % (msg.topic, msg.payload))
