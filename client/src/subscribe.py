79b8a3709a88c5994c0d649955a3ded9d6c2cd95
isangeeth95 <isangeeth95@gmail.com>
2021-04-25 14:43:58 -0400

    change subscribe.py protocol version into 5

--- client/src/subscribe.py
+++ client/src/subscribe.py
@@ -1,5 +1,5 @@
 #import paho.mqtt.subscribe as subscribe
 from paho.mqtt import subscribe as subscribe
 
-msg = subscribe.simple("house/main-light", hostname="10.0.2.4")
+msg = subscribe.simple("house/main-light", hostname="10.0.2.4", client_id="sangeeth")
 print("%s %s" % (msg.topic, msg.payload))
