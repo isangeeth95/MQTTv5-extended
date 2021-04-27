58b21381caa179bc11dd41a418f518afa3ae11f0
isangeeth95 <isangeeth95@gmail.com>
2021-04-27 06:30:32 -0400

    Logging info time updated into UTC

--- broker/interoperability/startbroker.py
+++ broker/interoperability/startbroker.py
@@ -16,13 +16,15 @@
 *******************************************************************
 """
 
-import mqtt, sys, logging
+import mqtt, sys, logging, time
 
 if __name__ == "__main__":
   formatter = logging.Formatter(fmt='%(levelname)s %(asctime)s %(message)s',  datefmt='%Y%m%d %H%M%S')
   ch = logging.StreamHandler()
   ch.setFormatter(formatter)
   ch.setLevel(logging.DEBUG)
+#  logging.basicConfig(format='%(asctime)s - %(message)s', datefmt='%d-%b-%y %H:%M:%S', level=logging.INFO)
+#  logging.Formatter.converter=time.gmtime
   broker_logger = logging.getLogger('MQTT broker')
   broker_logger.addHandler(ch)
   broker_logger.propagate = False # don't pass log entries up to the root logger
